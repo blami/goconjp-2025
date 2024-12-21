91602d75cdf4ef8c05862897e266510becb2a5db
Nozomu Ikuta <16436160+NozomuIkuta@users.noreply.github.com>
2024-12-22 02:10:06 +0900

    chore: set up github workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://gocon.github.io",
+  base: "/2025",
+  i18n: {
+    defaultLocale: "ja",
+    locales: ["ja"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+});
