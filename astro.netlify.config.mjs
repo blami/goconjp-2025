4c030c2b998f8d80b801d972488d0f24385bc31e
masakurapa <rabasan1031@gmail.com>
2025-05-23 21:37:09 +0900

    update astro.netlify.config.mjs

--- astro.netlify.config.mjs
+++ astro.netlify.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://gocon.github.io",
-  base: "/2025",
   i18n: {
     defaultLocale: "ja",
     locales: ["ja"],
