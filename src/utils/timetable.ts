c547e5f682b535f09a24b8d6c805fb782572cf31
Ryuichi Kataoka <r.kataoka.ps.ai@gmail.com>
2025-08-23 23:29:33 +0900

    💄 セッションが時間帯を跨ぐ時の表示に対応

--- src/utils/timetable.ts
+++ src/utils/timetable.ts
@@ -1,4 +1,22 @@
+import timetable from "../data/timetable.json";
 import speakers from "../data/speakers.json";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+/**
+ * 指定した開催日のタイムテーブルを取得
+ * @param eventDate イベント開催日
+ * @returns 指定した開催日のタイムテーブル
+ */
+export const getCurrentDateTimetable = (eventDate: string) =>
+  timetable.find(
+    (event) =>
+      dayjs(event.date).tz("Asia/Tokyo").format("YYYY-MM-DD") === eventDate,
+  );
 
 /**
  * 日時を時間フォーマットに変換
@@ -20,4 +38,74 @@ export const getFormattedTime = (time: string) => {
  */
 export const getSessionSpeaker = (speakerId?: string) => {
   return speakers.find((speaker) => speaker.id === speakerId);
+};
+
+/**
+ * 跨いだタイムテーブルの時間帯の行数を取得
+ * @param eventDate イベント開催日
+ * @param start セッションの開始時間
+ * @param end セッションの終了時間
+ * @returns 時間帯を跨いだ行数
+ */
+export const getTimeSlotSpanCount = (
+  eventDate: string,
+  start: string | undefined,
+  end: string | undefined,
+) => {
+  // 指定した開催日のタイムテーブルを取得
+  const currentDateTimeSlots =
+    getCurrentDateTimetable(eventDate)?.timeSlots ?? [];
+
+  // セッションの開始時間と終了時間から時間帯のインデックスを取得
+  const startIdx = currentDateTimeSlots.findIndex(
+    (slot) =>
+      slot.slotStart === dayjs(start).tz("Asia/Tokyo").format("HH:mm:ss"),
+  );
+  const endIdx = currentDateTimeSlots.findIndex(
+    (slot) => slot.slotStart === dayjs(end).tz("Asia/Tokyo").format("HH:mm:ss"),
+  );
+
+  // どちらも見つからない場合は0を返す
+  if (startIdx === -1 || endIdx === -1) return 0;
+
+  // 時間帯をまたぐ場合は、時間帯を跨いだ行数を計算
+  return Math.max(0, endIdx - startIdx);
+};
+
+/**
+ * 指定した時間帯が前の時間帯で占有されているかどうか
+ * @param eventDate イベント開催日
+ * @param roomId 会場ID
+ * @param timeSlot 時間帯
+ * @returns 今の時間帯が前の時間帯で占有されている場合はtrue
+ */
+export const isTimeSlotOccupied = (
+  eventDate: string,
+  roomId: number,
+  timeSlot: string,
+) => {
+  // 指定した開催日のタイムテーブルを取得
+  const currentDateTimeSlots =
+    getCurrentDateTimetable(eventDate)?.timeSlots ?? [];
+
+  // 指定した時間帯のインデックスを取得
+  const slotIndex = currentDateTimeSlots.findIndex(
+    (slot) => slot.slotStart === timeSlot,
+  );
+
+  if (slotIndex === -1) return false;
+
+  // 指定した時間帯の前までのタイムテーブルを取得
+  const timeSlots = currentDateTimeSlots.slice(0, slotIndex);
+
+  // 指定した会場の中でセッションの終了時間が指定した時間帯よりも後のものがあるか確認
+  return timeSlots.some((slot) =>
+    slot.rooms.some(
+      (r) =>
+        r.id === roomId &&
+        dayjs(r.session.endsAt)
+          .tz("Asia/Tokyo")
+          .isAfter(dayjs(`${eventDate}T${timeSlot}`).tz("Asia/Tokyo")),
+    ),
+  );
 };
