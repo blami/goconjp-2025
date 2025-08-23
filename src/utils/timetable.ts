fcdd55ebdcd86783b673877536ea9c690b7a400b
Ryuichi Kataoka <r.kataoka.ps.ai@gmail.com>
2025-08-23 23:29:33 +0900

    ✨ 現在のセッション情報でタイムテーブルページを生成

--- src/utils/timetable.ts
+++ src/utils/timetable.ts
@@ -1,35 +1,4 @@
-import sessions from "../data/sessions.json";
-
-/**
- * セッションのラベルを取得
- * @param session セッション
- * @param index セッションのインデックス
- * @returns セッションのラベル
- */
-export const getSessionLabel = (
-  session: (typeof sessions.sessions)[0],
-  index: number,
-) => {
-  const roomType = session.roomId === sessions.rooms.at(0)?.id ? "A" : "B";
-  // TODO: セッションのラベルを取得
-  return `${roomType}${index + 1}-SP`;
-};
-
-/**
- * セッションの時間区分を取得
- * @param sessionId セッションのID
- * @returns セッションの時間区分
- */
-export const getSessionTime = (sessionId: number[]) => {
-  // セッションのタイプを取得
-  const sessionType = sessions.categories.find(
-    (category) => category.title === "Session Type",
-  );
-
-  return (
-    sessionType?.items.find((item) => sessionId.includes(item.id))?.name || ""
-  );
-};
+import speakers from "../data/speakers.json";
 
 /**
  * 日時を時間フォーマットに変換
@@ -45,34 +14,10 @@ export const getFormattedTime = (time: string) => {
 };
 
 /**
- * セッションの部屋区分を取得
- * @param roomId 部屋のID
- * @returns 部屋の名前
- */
-export const getSessionRoom = (roomId: number) => {
-  return sessions.rooms.find((room) => room.id === roomId)?.name || "";
-};
-
-/**
- * セッションの登壇者名を取得
+ * 登壇者のプロフィール情報を取得
  * @param speakerId 登壇者のID
- * @returns 登壇者の名前
+ * @returns 登壇者
  */
-export const getSessionSpeakerName = (speakerId?: string) => {
-  return (
-    sessions.speakers.find((speaker) => speaker.id === speakerId)?.fullName ||
-    ""
-  );
-};
-
-/**
- * 登壇者のプロフィール画像を取得
- * @param speakerId 登壇者のID
- * @returns 登壇者のプロフィール画像
- */
-export const getSessionSpeakerThumbnailSrc = (speakerId?: string) => {
-  return (
-    sessions.speakers.find((speaker) => speaker.id === speakerId)
-      ?.profilePicture || ""
-  );
+export const getSessionSpeaker = (speakerId?: string) => {
+  return speakers.find((speaker) => speaker.id === speakerId);
 };
